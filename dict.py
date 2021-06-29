084698d61bcaf0b0b0e99e12507e991323089454
lucas-chang-code <85960603+lucas-chang-code@users.noreply.github.com>
2021-06-28 18:23:03 -0700

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,15 @@
+dict_name = {"chicken":5, "beef":6, "cheese":2, "milk":4.50}
+
+mushrooms = {"morels": 7, "portabella": 5, "chanterell": 2}
+
+morels = mushrooms["morels"]
+print(morels)
+
+fancy_shoes = {"Jordan_13":1, "Yeezy":8,"Foamposite":10,"Air_Max":5,"SB_Dunk":20}
+
+customer = 2 * fancy_shoes["SB_Dunk"] #step Vll
+print(customer)
+
+del fancy_shoes["Jordan_13"]
+del mushrooms["morels"]
+del dict_name["chicken"]
